0d4acf680c63899a284e650c9a6b809367d517b8
unnamed877 <80766560+unnamed877@users.noreply.github.com>
2021-03-22 17:26:15 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_certus.mk
+    $(LOCAL_DIR)/rr_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_certus-user \
-    nad_certus-userdebug \
-    nad_certus-eng
+    rr_certus-user \
+    rr_certus-userdebug \
+    rr_certus-eng
