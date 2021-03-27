b3eee5154f3222ea6ab0fb81d997f4cf556a3222
unnamed877 <80766560+unnamed877@users.noreply.github.com>
2021-03-27 09:11:50 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_certus.mk
+    $(LOCAL_DIR)/fluid_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_certus-user \
-    bliss_certus-userdebug \
-    bliss_certus-eng
+    fluid_certus-user \
+    fluid_certus-userdebug \
+    fluid_certus-eng
