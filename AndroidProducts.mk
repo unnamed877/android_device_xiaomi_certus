5ee4bcdf5d95b4eb966478eb6d06f81feda813be
unnamed877 <80766560+unnamed877@users.noreply.github.com>
2021-03-28 22:22:04 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_certus.mk
+    $(LOCAL_DIR)/corvus_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_certus-user \
-    fluid_certus-userdebug \
-    fluid_certus-eng
+    corvus_certus-user \
+    corvus_certus-userdebug \
+    corvus_certus-eng
