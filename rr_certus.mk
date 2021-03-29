8590809dc72951d84bdbed9e19ea0dcfe2fb0050
unnamed877 <80766560+unnamed877@users.noreply.github.com>
2021-03-29 12:22:36 +0800

    Update rr_certus.mk

--- rr_certus.mk
+++ rr_certus.mk
@@ -18,14 +18,14 @@
 $(call inherit-product, device/xiaomi/certus/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := certus
-PRODUCT_NAME := rr_certus
+PRODUCT_NAME := bliss_certus
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi 6 / Redmi 6A
 PRODUCT_MANUFACTURER := Xiaomi
