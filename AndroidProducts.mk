22630c94b4583d802392f1a4c7e7eaf5c4996d2a
unnamed877 <80766560+unnamed877@users.noreply.github.com>
2021-04-01 16:32:53 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/exthm_certus.mk
+    $(LOCAL_DIR)/fluid_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    exthm_certus-user \
-    exthm_certus-userdebug \
-    exthm_certus-eng
+    fluid_certus-user \
+    fluid_certus-userdebug \
+    fluid_certus-eng
